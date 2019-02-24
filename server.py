3672fde15745274e2a2a20c00ca02ee4db15a12d
terix2k11 <terixmail@googlemail.com>
2019-02-24 12:08:41 +0100

    handles basic commodities request

--- server.py
+++ server.py
@@ -7,32 +7,46 @@ HOST_NAME = "localhost"
 PORT_NUMBER = 8000
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_HEAD(self):
-    	print "do_HEAD"
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-    def do_GET(self):
-    	print "do_GET"
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        self.send_header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-        self.end_headers()
-        self.wfile.write("heyyy")
+	def setCORSHeader(self):
+		self.send_header('Access-Control-Allow-Origin', '*')
+		self.send_header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+		self.send_header('Access-Control-Allow-Methods', 'GET') # ,PUT,POST,DELETE	 
+		self.end_headers()
 
+	def do_HEAD(self):
+		print "do_HEAD"
+		self.send_response(200)
+		self.send_header("Content-type", "text/plain")
+		self.end_headers()
+	def do_GET(self):
+		print "Handles GET request:"
+
+		if(self.path == "/commodities"):
+			self.send_response(200)
+			self.send_header("Content-type", "text/json")
+			self.setCORSHeader()
+			self.wfile.write(commoditiesJSON)
+		else:
+			self.send_response(500)
+			self.send_header("Content-type", "text/plain")
+			self.setCORSHeader()
+			self.wfile.write("Access denied.")
 
 if __name__ == "__main__":
+	print "Loading assets:"
+	commoditiesJSON = "{ name: 'cheese'}"
 
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+	print "Starting Server"
 
-    try:
-            httpd.serve_forever()
-    except KeyboardInterrupt:
-            pass
 
-    httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+	server_class = BaseHTTPServer.HTTPServer
+	httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+	print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+
+	try:
+			httpd.serve_forever()
+	except KeyboardInterrupt:
+			pass
+
+	httpd.server_close()
+	print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
