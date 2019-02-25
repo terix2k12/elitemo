45978330979f5a7e3cef6cdda9e589eee29cd5f5
terix2k11 <terixmail@googlemail.com>
2019-02-25 18:10:21 +0100

    remove missions dynamically

--- elite.js
+++ elite.js
@@ -4,13 +4,19 @@
 		jsCheck.innerHTML = "";
   	}
 
+	function removeMission(element) {
+		var oldDiv = element.parentNode;
+		oldDiv.parentNode.removeChild(oldDiv);		
+	}
+  	
   	function addMission(element) {
   		var newMission = "";
 		newMission += "	Type: <input list='missionDropdown' name='mission'>";
 		newMission += "	Commodity: <input class='commoditiesAutocomplete' name='commodity'>";
 		newMission += "	Target: <input class='stationsAutocomplete' name='commodity'/>";
 		newMission += "	Tonnes: <input name='commodity'/>";
-		newMission += "	Value: <input name='commodity'/>";
+		// newMission += "	Value: <input name='commodity'/>";
+		newMission += " <button onClick='removeMission(this)' type='button'>Remove</button>";
 
 		var newDiv = document.createElement('div');
 		newDiv.innerHTML = newMission;
