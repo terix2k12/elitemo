31321743060ceba21dcc428b53ad34557ce28652
terix2k12 <terixmail@googlemail.com>
2019-03-02 10:27:47 +0100

    fix server

--- elite.py
+++ elite.py
@@ -29,21 +29,21 @@ class elite:
 			if( sys[u'name'] == name ) or int(sys[u'id']) == id:
 				return sys
 	
-#	def findSystemLike(self, name):
-#		return self.findNameLike(name, self.systems)
-#	
-#	def findStationLike(self, name):
-#		return self.findNameLike(name, self.stations)
-#	
-#	def findCommoditiyLike(self, name):
-#		return self.findNameLike(name, self.commodities)
-#	
-#	def findNameLike(self, name, items):
-#		result = []
-#		for item in items: 
-#			if( self.simple(item[u'name']).find(self.simple(name)) >= 0):
-#				result.append(item)
-#		return result
+	def findSystemLike(self, name):
+		return self.findNameLike(name, self.systems)
+	
+	def findStationLike(self, name):
+		return self.findNameLike(name, self.stations)
+	
+	def findCommoditiyLike(self, name):
+		return self.findNameLike(name, self.commodities)
+	
+	def findNameLike(self, name, items):
+		result = []
+		for item in items: 
+			if( self.simple(item[u'name']).find(self.simple(name)) >= 0):
+				result.append(item)
+		return result
 	
 	def simple(self, string):
 		s = string.upper().replace(" ", "").replace("+","")
@@ -114,35 +114,13 @@ class elite:
 if __name__ == "__main__":
 	print "Start Elite:Dangerous Mission Optimizer"
 
-	
-	self.stationsPath = "stations.json"
-	self.systemsPath = "systems_populated.json"
-	self.commoditiesPath = "commodities.json"
-	self.marketsPath = "blubb"
-
 	elite = elite()
 
-	# (elite.commodities, elite.systems, elite.stations) = assets.loadAssets()
+	elite.stations = assets.stations("stations.json")
+	elite.systems = assets.systems("systems_populated.json")
+	elite.markets = assets.markets("listings.csv")
+	elite.commodities = assets.commodities("commodities.json") 
 
 	server(elite).runServer()
 
 	print "End Elite:D-MO"
-
-#	sid = 0
-#
-#	eravate = findSystem("Eravate")
-#	lhs = findSystem("LHS 3447")
-#
-#	print findSystemLike("rava")
-#
-#	sid = eravate[u'id']
-#
-#	for sta in stations:
-#		if( sta[u'system_id'] == sid):
-#			print "\t", sta[u'name'], sta[u'id']
-#
-#	for sys in systems:
-#		if( distance(sys, eravate) < 10):
-#			print sys[u'name']
-#
-#	print "Done"
