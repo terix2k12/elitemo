f5e43e0e6693d90ca7d419146badff10664d98ac
terix2k12 <terixmail@googlemail.com>
2019-03-02 09:38:12 +0100

    assets as module, not class

--- assets.py
+++ assets.py
@@ -1,42 +1,40 @@
 import json
 import csv
 
-class assets:
+def loadJSON(name, path):
+	print "Loading " + name + ":"
+	f = open(path,"r")
+	j = json.load(f)
+	print str(len(j)) + " " + name + " parsed from JSON"
+	f.close()
+	return j
 
-	def loadJSON(self, name, path):
-		print "Loading " + name + ":"
-		f = open(path,"r")
-		j = json.load(f)
-		print str(len(j)) + " " + name + " parsed from JSON"
-		f.close()
-		return j
+def loadCSV(path):
+	print "Loading " + path + ":"
+	f = open(path, "r")
+	content = []
+	csvrows = csv.DictReader(f, delimiter=",")
+	for row in csvrows:
+		content.append(row)
+	print str(len(content)) + " parsed from CSV"
+	return content
 
-	def loadCSV(self, path):
-		print "Loading " + path + ":"
-		f = open(path, "r")
-		content = []
-		csvrows = csv.DictReader(f, delimiter=",")
-		for row in csvrows:
-			content.append(row)
-		print str(len(content)) + " parsed from CSV"
-		return content
+def commodities(path):
+	return loadJSON("commodities", path)
 
-	def commodities(self, path):
-		return self.loadJSON("commodities", path)
+def systems(path):
+	return loadJSON("systems", path)
 
-	def systems(self, path):
-		return self.loadJSON("systems", path)
+def stations(path):
+	return loadJSON("stations", path)
 
-	def stations(self, path):
-		return self.loadJSON("stations", path)
-
-	def markets(self, path):
-		items = self.loadCSV(path)
-		markets = {}
-		for item in items:
-			marketId = int(item["station_id"])
-			if marketId not in markets:
-				markets[marketId] = []
-			market = markets[marketId]
-			market.append(item) 
-		return markets
+def markets(path):
+	items = loadCSV(path)
+	markets = {}
+	for item in items:
+		marketId = int(item["station_id"])
+		if marketId not in markets:
+			markets[marketId] = []
+		market = markets[marketId]
+		market.append(item) 
+	return markets
--- test_assets.py
+++ test_assets.py
@@ -1,30 +1,27 @@
 import unittest
-from assets import assets
+import assets
 
 class TestAsset(unittest.TestCase):
 
-	def setUp(self):
-		self.assets = assets()
-
 	def test_assets_markets(self):
-		markets = self.assets.markets("test/test-markets.csv")
+		markets = assets.markets("test/test-markets.csv")
 		self.assertEqual(len(markets), 2)
 
 	def test_assets_commodities(self):
-		commodities = self.assets.commodities("commodities.json")
+		commodities = assets.commodities("commodities.json")
 		self.assertEqual(len(commodities), 355)
 
 	def test_assets_systems(self):
-		systems = self.assets.systems("test/test-systems.json")
+		systems = assets.systems("test/test-systems.json")
 		# print [s["id"] for s in systems]
 		self.assertEqual(len(systems), 4)		
 
 	def test_assets_stations(self):
-		stations = self.assets.systems("test/test-stations.json")
+		stations = assets.systems("test/test-stations.json")
 		# print [s["name"] for s in stations]
 		self.assertEqual(len(stations), 20)
 
 	# make this somehow only executed in SLOW tests
 	def slow_test_assets_markets_big(self):
-		markets = self.assets.markets("listings.csv")
+		markets = assets.markets("listings.csv")
 		self.assertEqual(len(markets), 53899)
